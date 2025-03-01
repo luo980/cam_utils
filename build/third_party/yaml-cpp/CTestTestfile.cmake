3eec77450935ab25c0d1b1d2939b296df9b05edb
luo980 <luo980@gmail.com>
2025-03-01 14:29:10 +0800

    my 1.0 cam utils

--- build/third_party/yaml-cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luo980/gits/cam_utils/third_party/yaml-cpp
-# Build directory: /home/luo980/gits/cam_utils/build/third_party/yaml-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("util")
--- build/third_party/yaml-cpp/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luo980/gits/cam_utils/third_party/yaml-cpp/util
-# Build directory: /home/luo980/gits/cam_utils/build/third_party/yaml-cpp/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
